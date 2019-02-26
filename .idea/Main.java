0f33fa6c4175c85c3d1840421606c9a97068022d
dcndaniela <diaconudaniela@yahoo.com>
2019-02-26 09:29:15 +0200

    test commit

---
+++ .idea/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;//un fel de include;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! Say something");
+        //int a = 5, b = 10;
+        //int sum = a + b;
+      //  System.out.println("Suma numerelor " + a + " si " + b + " este: " + sum); // + concateneaza stringuri
+        Scanner scanner= new Scanner(System.in); // se creeaza obiectul scanner
+     String ceAmCititDinConsola= scanner.nextLine();//cu nextLine citesc toata linia pana la Enter
+     System.out.println("Am citit:"+ceAmCititDinConsola);
+
+
+    //citim actiuni: 0-exit, 1-suma, 2-diferenta
+
+    System.out.println("citim actiuni:0-exit, 1-suma, 2-diferenta");
+            int CeAmCititDinScanner=scanner.nextInt();
+            if(CeAmCititDinScanner==0)
+                System.out.println("am citit exit");
+            else
+                System.out.println("Am citit altceva");
+
+
+
+
+
+    }
+}
